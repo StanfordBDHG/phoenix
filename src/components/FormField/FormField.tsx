d96c0138cbc28c902ab2e7cce24c6ee2eb66aafe
Vishnu Ravi <vishnur@stanford.edu>
2025-05-31 11:58:41 -0400

    Show tooltip on hover or click

--- src/components/FormField/FormField.tsx
+++ src/components/FormField/FormField.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import HelpIcon from '../../images/icons/help-circle-outline.svg';
 
@@ -12,6 +12,7 @@ type Props = {
 
 const FormField = ({ label, sublabel, tooltip, isOptional, children }: Props): JSX.Element => {
     const { t } = useTranslation();
+    const [showTooltip, setShowTooltip] = useState(false);
 
     return (
         <div className="form-field">
@@ -20,18 +21,58 @@ const FormField = ({ label, sublabel, tooltip, isOptional, children }: Props): J
                     <span>{label}</span>
                     {isOptional && <span className="form-field__optional">{` (${t('Optional')})`}</span>}
                     {tooltip && (
-                        <img 
-                            src={HelpIcon}
-                            alt="Help"
-                            title={tooltip}
-                            style={{
-                                marginLeft: '2px',
-                                marginTop: '2px',
-                                width: '15px',
-                                height: '15px',
-                                cursor: 'help'
-                            }}
-                            />
+                        <span style={{ position: 'relative', display: 'inline-block' }}>
+                            <img 
+                                src={HelpIcon}
+                                alt="Help"
+                                style={{
+                                    marginLeft: '2px',
+                                    marginTop: '2px',
+                                    width: '15px',
+                                    height: '15px',
+                                    cursor: 'help'
+                                }}
+                                onMouseEnter={() => setShowTooltip(true)}
+                                onMouseLeave={() => setShowTooltip(false)}
+                                onClick={() => setShowTooltip(!showTooltip)}
+                                />
+                            {showTooltip && (
+                                <div
+                                    style={{
+                                        position: 'absolute',
+                                        bottom: '100%',
+                                        left: '50%',
+                                        transform: 'translateX(-50%)',
+                                        marginBottom: '5px',
+                                        padding: '8px 12px',
+                                        backgroundColor: '#333',
+                                        color: 'white',
+                                        fontSize: '12px',
+                                        borderRadius: '4px',
+                                        zIndex: 1000,
+                                        boxShadow: '0 2px 8px rgba(0,0,0,0.15)',
+                                        minWidth: '200px',
+                                        maxWidth: '500px',
+                                        whiteSpace: 'normal'
+                                    }}
+                                >
+                                    {tooltip}
+                                    <div
+                                        style={{
+                                            position: 'absolute',
+                                            top: '100%',
+                                            left: '50%',
+                                            transform: 'translateX(-50%)',
+                                            width: 0,
+                                            height: 0,
+                                            borderLeft: '5px solid transparent',
+                                            borderRight: '5px solid transparent',
+                                            borderTop: '5px solid #333'
+                                        }}
+                                    />
+                                </div>
+                            )}
+                        </span>
                     )}
                 </label>
             )}
